e791eed91d86e0c950ccc787872799542976534e
RenatoEng96 <renato96.ram@gmail.com>
2026-05-30 14:05:47 -0300

    PWA
    Inicio do PWA

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'rachador-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/Icone.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return from cache
+        }
+        return fetch(event.request).catch(() => {
+          // Fallback if offline and not in cache
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
